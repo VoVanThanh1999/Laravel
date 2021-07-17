71792f9ab440b70c0260548bd5ceebdc3c30470e
vietnd1 <nguyendinhviet0203@gmail.com>
2021-07-17 21:39:43 +0700

    update

--- ShopBanVe/resources/views/UserHome/HomePage/index.blade.php
+++ ShopBanVe/resources/views/UserHome/HomePage/index.blade.php
@@ -295,88 +295,158 @@
             </div> <!-- End content Area class -->
 
 
-            <div class="footer-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="footer-top-address wow slideInLeft" data-wow-duration="1s" data-wow-delay="0s"
-                                data-wow-offset="10">
-                                <div class="header-logo"
-                                    style=" text-align: center;border-bottom: 1px dotted rgba(247, 12, 38, 0.24);">
-                                    <a href="index.html">
-                                        <img src="{{ asset('img/logo.png') }}" alt="Your Shop Logo" class="img img-responsive"
-                                            style="max-width: 100px">
-                                    </a>
-                                </div>
-                                <div class="clearfix"></div>
-                                <ul>
-                                    <li><i class="fa fa-map-marker"></i> Patuakhali Science & Technology University,
-                                        Dumki-8602, Patuakhali</li>
-                                    <li><i class="fa fa-mobile"></i> +8801951233084 </li>
-                                    <li><i class="fa fa-phone"></i> +222 11 4444 </li>
-                                    <li><i class="fa fa-envelope-o"></i> <a href="mailto:manirujjamanakash@gmail.com">
-                                            manirujjamanakash@gmail.com</a></li>
-                                </ul>
-                            </div>
-
-
-                        </div>
-                        <div class="col-md-8">
-                            <div class="subscribe wow slideInRight" data-wow-duration="1s" data-wow-delay="0s"
-                                data-wow-offset="10">
-                                <h3>Subscribe here to get some exciting offers</h3>
-                                <form action="#" method="post">
-                                    <input type="text" name="email" placeholder="Enter your Email..." required="">
-                                    <input type="submit" value="Subscribe">
-                                </form>
-                            </div>
-                            <div class="all-footer-links">
-                                <div class="col-md-4">
-                                    <h3>Company</h3>
-                                    <ul>
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                        <li><a href="privacy.html">Privacy Policy</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-4 footer-grids">
-                                    <h3>Services</h3>
-                                    <ul>
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                        <li><a href="login.html">Returns</a></li>
-                                        <li><a href="faq.html" class="link">FAQ</a></li>
-                                        <li><a href="#">Site Map</a></li>
-                                        <li><a href="login.html">Order Status</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-4 footer-grids">
-                                    <h3>Payment Methods</h3>
-                                    <ul>
-                                        <li><i class="fa fa-paypal" aria-hidden="true"></i> Paypal</li>
-                                        <li><i class="fa fa-money" aria-hidden="true"></i> Bkash</li>
-                                        <li><i class="fa fa-pie-chart" aria-hidden="true"></i>EMI Conversion</li>
-                                        <li><i class="fa fa-gift" aria-hidden="true"></i> E-Gift Voucher</li>
-                                        <li><i class="fa fa-credit-card" aria-hidden="true"></i> Debit/Credit Card</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="payment-links">
-                                <ul>
-                                    <li><i class="fa fa-cc-paypal" style="color: #FFCC80"></i></li>
-                                    <li><i class="fa fa-cc-mastercard" style="color: #FFEB3B"></i></li>
-                                    <li><i class="fa fa-cc-stripe" style="color: yellow"></i></li>
-                                    <li><i class="fa fa-credit-card" style="color: #FF9800"></i></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                </div>
-            </div> <!-- End Footer top -->
-
+           <!-- -------- START FOOTER 1 w/ COMPANY DESCRIPTION AND 4 COLS ------- -->
+<footer class="footer py-9">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 mb-5 mb-lg-0">
+          <h6 class="text-uppercase mb-2">Soft</h6>
+          <p class="mb-4 pb-2">
+              The next generation of design systems.
+          </p>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-3">
+            <span class="text-lg fab fa-facebook"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-3">
+            <span class="text-lg fab fa-twitter"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-3">
+            <span class="text-lg fab fa-instagram"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-3">
+            <span class="text-lg fab fa-pinterest"></span>
+          </a>
+          <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-3">
+            <span class="text-lg fab fa-github"></span>
+          </a>
+        </div>
+        <div class="col-md-2 col-6 ms-lg-auto mb-md-0 mb-4">
+          <h6 class="text-sm">Company</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                About Us
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Careers
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Press
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </div>
+  
+        <div class="col-md-2 col-6 mb-md-0 mb-4">
+          <h6 class="text-sm">Pages</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Login
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Register
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Add list
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+  
+        <div class="col-md-2 col-6 mb-md-0 mb-4">
+          <h6 class="text-sm">Legal</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Terms
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                About Us
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Team
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Privacy
+              </a>
+            </li>
+          </ul>
+        </div>
+  
+        <div class="col-md-2 col-6 mb-md-0 mb-4">
+          <h6 class="text-sm">Resources</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Blog
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Service
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Product
+              </a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.creative-tim.com" target="_blank">
+                Pricing
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr class="horizontal dark mt-lg-5 mt-4 mb-sm-4 mb-1">
+      <div class="row">
+        <div class="col-8 mx-lg-auto text-lg-center">
+          <p class="text-sm text-secondary">
+            Copyright © <script>document.write(new Date().getFullYear())</script> Soft by Creative Tim.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- -------- END FOOTER 1 w/ COMPANY DESCRIPTION AND 4 COLS ------- -->
             <div class="footer-bottom">
                 <p class="footer-credit">
                     <script type="text/javascript">
